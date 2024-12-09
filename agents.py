1558ad865e8111df8f36bb29ef794d4f913979fd
Vivek Keval <vivek.keval@utexas.edu>
2024-12-08 23:57:02 -0600

    Created Base Point for different observation states

---
+++ agents.py
@@ -0,0 +1,120 @@
+import numpy as np
+from stable_baselines3 import A2C
+from gymnasium.spaces import Box
+from poke_env.data import GenData
+
+from poke_env.player import Gen9EnvSinglePlayer, RandomPlayer
+from poke_env.environment import Battle
+import time
+
+GEN_DATA = GenData.from_gen(9)
+
+
+class SimpleRLPlayer0(Gen9EnvSinglePlayer):
+    def embed_battle(self, battle: Battle):
+
+        # -1 indicates that the move does not have a base power
+        # or is not available
+        moves_base_power = -np.ones(4)
+        moves_dmg_multiplier = np.ones(4)
+
+        for i, move in enumerate(battle.available_moves):
+            battle.active_pokemon.stab_multiplier
+            moves_base_power[i] = (
+                move.base_power / 100
+            )  # Simple rescaling to facilitate learning
+            if move.type:
+                moves_dmg_multiplier[i] = move.type.damage_multiplier(
+                    battle.opponent_active_pokemon.type_1,
+                    battle.opponent_active_pokemon.type_2,
+                    type_chart=GEN_DATA.type_chart,
+                )
+
+        # We count how many pokemons have not fainted in each team
+        remaining_mon_team = (
+            len([mon for mon in battle.team.values() if mon.fainted]) / 6
+        )
+        remaining_mon_opponent = (
+            len([mon for mon in battle.opponent_team.values() if mon.fainted]) / 6
+        )
+
+        # Final vector with 10 components
+        return np.concatenate(
+            [
+                moves_base_power,
+                moves_dmg_multiplier,
+                [remaining_mon_team, remaining_mon_opponent],
+            ]
+        )
+
+    def calc_reward(self, last_state, current_state) -> float:
+        return self.reward_computing_helper(
+            current_state, fainted_value=2, hp_value=1, victory_value=30
+        )
+
+    def describe_embedding(self):
+        low = [-1, -1, -1, -1, 0, 0, 0, 0, 0, 0]
+        high = [3, 3, 3, 3, 4, 4, 4, 4, 1, 1]
+
+        return Box(
+            np.array(low, dtype=np.float32),
+            np.array(high, dtype=np.float32),
+            dtype=np.float32,
+        )
+
+
+# We define our RL player
+# It needs a state embedder and a reward computer, hence these two methods
+class SimpleRLPlayer(Gen9EnvSinglePlayer):
+    def embed_battle(self, battle: Battle):
+
+        dmg_multipliers = np.zeros(24)
+        base_powers = np.zeros(24)
+        for i, pokemon in enumerate(battle.team.values()):
+            if not pokemon.fainted:
+                for j, move in enumerate(pokemon.moves.values()):
+                    stab_mult = max(
+                        (move.type in pokemon.types) * pokemon.stab_multiplier, 1
+                    )
+                    dmg_multipliers[4 * i + j] = (
+                        move.type.damage_multiplier(
+                            battle.opponent_active_pokemon.type_1,
+                            battle.opponent_active_pokemon.type_2,
+                            type_chart=GEN_DATA.type_chart,
+                        )
+                        * stab_mult
+                    )
+                    base_powers[4 * i + j] = move.base_power / 100
+        return np.concatenate([dmg_multipliers, base_powers])
+
+    def calc_reward(self, last_state, current_state) -> float:
+        return self.reward_computing_helper(
+            current_state, fainted_value=2, hp_value=1, victory_value=30
+        )
+
+    def describe_embedding(self):
+        low_mult = np.zeros(24)
+        high_mult = np.ones(24) * 10
+        low_base = -np.ones(24)
+        high_base = np.ones(24) * 4
+        low = np.concatenate([low_mult, low_base])
+        high = np.concatenate([high_mult, high_base])
+
+        return Box(
+            np.array(low, dtype=np.float32),
+            np.array(high, dtype=np.float32),
+            dtype=np.float32,
+        )
+
+
+class MaxDamagePlayer(RandomPlayer):
+    def choose_move(self, battle):
+        # If the player can attack, it will
+        if battle.available_moves:
+            # Finds the best move among available ones
+            best_move = max(battle.available_moves, key=lambda move: move.base_power)
+            return self.create_order(best_move)
+
+        # If no attack is available, a random switch will be made
+        else:
+            return self.choose_random_move(battle)
--- rl_example.py
+++ rl_example.py
@@ -3,75 +3,13 @@ from stable_baselines3 import A2C
 from gymnasium.spaces import Box
 from poke_env.data import GenData
 
-from poke_env.player import Gen9EnvSinglePlayer, RandomPlayer
+from poke_env.player import RandomPlayer
+from agents import SimpleRLPlayer, MaxDamagePlayer
 import time
 
-# We define our RL player
-# It needs a state embedder and a reward computer, hence these two methods
-class SimpleRLPlayer(Gen9EnvSinglePlayer):
-    def embed_battle(self, battle):
-        # -1 indicates that the move does not have a base power
-        # or is not available
-        moves_base_power = -np.ones(4)
-        moves_dmg_multiplier = np.ones(4)
-        for i, move in enumerate(battle.available_moves):
-            moves_base_power[i] = (
-                move.base_power / 100
-            )  # Simple rescaling to facilitate learning
-            if move.type:
-                moves_dmg_multiplier[i] = move.type.damage_multiplier(
-                    battle.opponent_active_pokemon.type_1,
-                    battle.opponent_active_pokemon.type_2,
-                    type_chart=GEN_9_DATA.type_chart
+GEN_DATA = GenData.from_gen(9)
 
-                )
-
-        # We count how many pokemons have not fainted in each team
-        remaining_mon_team = (
-            len([mon for mon in battle.team.values() if mon.fainted]) / 6
-        )
-        remaining_mon_opponent = (
-            len([mon for mon in battle.opponent_team.values() if mon.fainted]) / 6
-        )
-
-        # Final vector with 10 components
-        return np.concatenate(
-            [
-                moves_base_power,
-                moves_dmg_multiplier,
-                [remaining_mon_team, remaining_mon_opponent],
-            ]
-        )
-
-    def calc_reward(self, last_state, current_state) -> float:
-        return self.reward_computing_helper(
-            current_state, fainted_value=2, hp_value=1, victory_value=30
-        )
-    
-    def describe_embedding(self):
-        low = [-1, -1, -1, -1, 0, 0, 0, 0, 0, 0]
-        high = [3, 3, 3, 3, 4, 4, 4, 4, 1, 1]
-        return Box(
-            np.array(low, dtype=np.float32),
-            np.array(high, dtype=np.float32),
-            dtype=np.float32,
-        )
-
-
-class MaxDamagePlayer(RandomPlayer):
-    def choose_move(self, battle):
-        # If the player can attack, it will
-        if battle.available_moves:
-            # Finds the best move among available ones
-            best_move = max(battle.available_moves, key=lambda move: move.base_power)
-            return self.create_order(best_move)
-
-        # If no attack is available, a random switch will be made
-        else:
-            return self.choose_random_move(battle)
-
-
-NB_TRAINING_STEPS = 10000
+NB_TRAINING_STEPS = 20000
 NB_EVALUATION_EPISODES = 100
 
 np.random.seed(0)
@@ -79,12 +17,12 @@ np.random.seed(0)
 
 model_store = {}
 
+
 # This is the function that will be used to train the a2c
 def a2c_training(player, nb_steps):
     model = A2C("MlpPolicy", player, verbose=1)
     model.learn(total_timesteps=10_000)
     model_store[player] = model
-    
 
 
 def a2c_evaluation(player, nb_episodes):
@@ -101,32 +39,48 @@ def a2c_evaluation(player, nb_episodes):
 
 NB_TRAINING_STEPS = 20_000
 TEST_EPISODES = 100
-GEN_9_DATA = GenData.from_gen(9)
 
 if __name__ == "__main__":
-    
+
     print("Init Players")
     opponent = RandomPlayer()
-    env_player = SimpleRLPlayer(opponent=opponent)
+    env_player = SimpleRLPlayer(
+        opponent=opponent,
+    )
     second_opponent = MaxDamagePlayer()
-    
+
     print("Make Model Learn")
     model = A2C("MlpPolicy", env_player, verbose=1)
+    print(model.action_space)
+    print(model.observation_space)
     model.learn(total_timesteps=NB_TRAINING_STEPS)
-
     obs, reward, done, _, info = env_player.step(0)
     while not done:
         action, _ = model.predict(obs, deterministic=True)
         obs, reward, done, _, info = env_player.step(action)
 
-    finished_episodes = 0
-
-    env_player.reset_battles()
+    all_battles_completed = False
+    while not all_battles_completed:
+        try:
+            env_player.reset_battles()
+            all_battles_completed = True
+        except EnvironmentError as e:
+            unfinished_battles = []
+            for battle in list(env_player.agent._battles.values()):
+                if not battle.finished:
+                    unfinished_battles.append(battle)
+            print(f"{unfinished_battles} are ongoing")
+            time.sleep(0.5)
     obs, _ = env_player.reset()
-    while True:
-        action, _ = model.predict(obs, deterministic=True)
-        obs, reward, done, _, info = env_player.step(action)
 
+    finished_episodes = 0
+    while True:
+        try:
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, done, _, info = env_player.step(action)
+        except RuntimeError as e:
+            print(f"{e} at finished episodes {finished_episodes}")
+            done = True
         if done:
             finished_episodes += 1
             if finished_episodes >= TEST_EPISODES:
@@ -143,12 +97,16 @@ if __name__ == "__main__":
     time.sleep(3)
 
     while True:
-        action, _ = model.predict(obs, deterministic=True)
-        obs, reward, done, _, info = env_player.step(action)
+        try:
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, done, _, info = env_player.step(action)
+        except RuntimeError as e:
+            print(f"{e} at finished episodes {finished_episodes}")
+            done = True
         if done:
             finished_episodes += 1
-            obs, _ = env_player.reset()
             if finished_episodes >= TEST_EPISODES:
                 break
+            obs, _ = env_player.reset()
 
     print("Won", env_player.n_won_battles, "battles against", env_player._opponent)
